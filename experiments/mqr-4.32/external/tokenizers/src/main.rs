c1fb35c63362b8fe638cead7c97392ee5bc52eef
OOZ OON" Kim <71053804+WhoSia@users.noreply.github.com>
2026-09-25 02:17:21 +0900

    MQR-4.32: rustfmt tokenizers external probe

--- experiments/mqr-4.32/external/tokenizers/src/main.rs
+++ experiments/mqr-4.32/external/tokenizers/src/main.rs
@@ -1,9 +1,7 @@
 use std::{env, process};
 
 use tokenizers::{
-    models::wordlevel::WordLevel,
-    pre_tokenizers::whitespace::Whitespace,
-    TokenizerBuilder,
+    TokenizerBuilder, models::wordlevel::WordLevel, pre_tokenizers::whitespace::Whitespace,
 };
 
 const BRIDGE: &[(&str, &str)] = &[
